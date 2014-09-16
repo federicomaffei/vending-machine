4821d390cdc5895adcecc8b563d7a0caf5cf300a
federicomaffei <federico.maffei@gmail.com>
2014-09-16 16:13:12 +0100

    added ability for vending machine to be initially loaded.

--- lib/vendingmachine.rb
+++ lib/vendingmachine.rb
@@ -1,7 +1,7 @@
 class VendingMachine
 
 	def initialize
-		@items = Array.new(100)
+		@items = Array.new(50)
 	end
 
 	def items
@@ -10,5 +10,17 @@ class VendingMachine
 
 	def empty?
 		items.count(nil) == items.length ? true : false
+	end
+
+	def initial_load
+		10.times {@items << Product.new('Mars Bar', 100, 1)}
+		10.times {@items << Product.new('Coca Cola', 70, 2)}
+		10.times {@items << Product.new('Snickers', 90, 3)}
+		10.times {@items << Product.new('Pringles', 120, 4)}
+		10.times {@items << Product.new('Water', 50, 5)}	
+	end
+
+	def item_count(name)
+		items.compact.select {|item| item.name == name}.count
 	end
 end
--- spec/vendingmachine_spec.rb
+++ spec/vendingmachine_spec.rb
@@ -1,13 +1,32 @@
 require 'vendingmachine'
 
 describe VendingMachine do
-	context 'items listing' do
+	context 'when initialized' do
 		let(:machine) {VendingMachine.new}
-		it 'has a capacity of 100 slots for items by default.' do
-			expect(machine.items.length).to eq 100
+		it 'has a capacity of 50 slots for items by default.' do
+			expect(machine.items.length).to eq 50
 		end
 		it 'when created it is empty.' do
 			expect(machine).to be_empty
+		end
+	end
+	context 'initial loading' do
+		let(:machine) {VendingMachine.new}
+		before(:each) {machine.initial_load}
+		it 'is initially loaded with 10 Mars Bars' do
+			expect(machine.item_count('Mars Bar')).to eq 10
+		end
+		it 'is initially loaded with 10 Snickers' do
+			expect(machine.item_count('Snickers')).to eq 10
+		end
+		it 'is initially loaded with 10 Coca Cola' do
+			expect(machine.item_count('Coca Cola')).to eq 10
+		end
+		it 'is initially loaded with 10 Pringles cans' do
+			expect(machine.item_count('Pringles')).to eq 10
+		end
+		it 'is initially loaded with 10 water bottles' do
+			expect(machine.item_count('Water')).to eq 10
 		end
 	end
 end
